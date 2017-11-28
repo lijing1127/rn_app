e01d240e87be634a966d53cab99d1e0426ee6401
zongwu <zongwu77@126.com>
2017-11-28 10:58:10 +0800

    三级联动

--- containers/shoppingmall/orderAndAddr/NewAddress.js
+++ containers/shoppingmall/orderAndAddr/NewAddress.js
@@ -6,9 +6,11 @@ import {
 	TextInput,
 	StyleSheet,
 	TouchableOpacity,
+	Alert,
 } from "react-native";
-
-import ModalDropdown from 'react-native-modal-dropdown';
+import { Picker, List, WhiteSpace } from 'antd-mobile';
+import { district, provinceLite as province } from 'antd-mobile-demo-data';
+import arrayTreeFilter from 'array-tree-filter';
 
 class UselessTextInput extends Component {
   constructor(props) {
@@ -32,52 +34,63 @@ export default class OrderAndAddr extends React.Component{
 	}
 	constructor(props) {
 		super(props);
+    this.state = {
+      data: [],
+      cols: 1,
+      pickerValue: [],
+      asyncValue: [],
+      visible: false,
+    }
 	}
+   getSel() {
+    const value = this.state.pickerValue;
+    if (!value) {
+      return '';
+    }
+    const treeChildren = arrayTreeFilter(district, (c, level) => c.value === value[level]);
+    return treeChildren.map(v => v.label).join(',');
+  }
 	render(){
 		return(
 			<View  style={{position:'relative',height:'100%',backgroundColor:gColor.whiteColor}}>
 				<ScrollView>
 					<View style={styles.listSty}>
 						<Text style={styles.txtSty}>收货人姓名：</Text>
-						<View style={{width:'75%',marginLeft:10}}>
+						<View style={{width:'75%',}}>
 							<UselessTextInput/>
 						</View>
 					</View>
 					<View style={styles.listSty}>
 						<Text style={styles.txtSty}>手机号码：</Text>
-						<View style={styles.width}>
+						<View style={{width:'75%',}}>
 							<UselessTextInput />
 						</View>
 					</View>
-					<View style={styles.listSty}>
-						<Text style={styles.txtSty}>省份：</Text>
-						<View style={styles.width}>
-							<ModalDropdown options={['北京', '上海','广东','河南','山东']} defaultValue="北京" 
-								style={styles.btnSty}
-								textStyle={styles.textStyle}
-								dropdownStyle={styles.dropdownStyle}
-								dropdownTextHighlightStyle={styles.dropdownTextHighlightStyle}/>
-						</View>
-					</View>
-
-					<View style={styles.listSty}>
-						<Text style={styles.txtSty}>城市：</Text>
-						<View style={styles.width}>
-							<ModalDropdown options={['北京', '上海','深圳','广州','济南']} defaultValue="北京" 
-								style={{borderWidth:1,borderColor:gColor.borderColors,height:40}}
-								textStyle={styles.textStyle}
-								dropdownStyle={styles.dropdownStyle}
-								dropdownTextHighlightStyle={styles.dropdownTextHighlightStyle}/>
-						</View>
-					</View>
-
-					<View style={styles.listSty}>
-						<Text style={styles.txtSty}>地区或街道：</Text>
-						<View style={styles.width}>
-							<UselessTextInput />
-						</View>
-					</View>
-			
+					 <List style={{marginTop: 15, borderWidth: 1, marginHorizontal: 15, borderColor: gColor.borderColors}}>
+               <Picker
+                visible={this.state.visible}
+                data={district}
+                value={this.state.pickerValue}
+                onChange={v => this.setState({ pickerValue: v })}
+                onOk={() => this.setState({ visible: false })}
+                onDismiss={() => this.setState({ visible: false })}
+              >
+                <List.Item extra={this.getSel()} onClick={() => this.setState({ visible: true })}>
+                  <Text>省份/市/区：</Text>
+                </List.Item>
+              </Picker>  
+           </List>
+           <View style={{margin: 15, flexDirection: "row"}}>
+             <Text>详细地址：</Text>
+             <TextInput 
+              placeholder="" 
+              style={{borderWidth: 1, flex: 1, borderColor: gColor.borderColors,}} 
+              underlineColorAndroid="transparent"
+              multiline={true}
+              numberOfLines={5}
+              maxLength={120}
+              />
+           </View>
 				</ScrollView>
 
 				<TouchableOpacity style={styles.add}>
